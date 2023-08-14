20a86c6bf9606b802d834de806a280df6f4bfad0
Yuri Haiduk <yurihaiduk@gmail.com>
2023-08-15 01:23:52 +0300

    hotfix

--- app/Contracts/Parser/ParserInterface.php
+++ app/Contracts/Parser/ParserInterface.php
@@ -6,15 +6,6 @@ use DiDom\Document;
 
 interface ParserInterface
 {
-    public function getHtmlPage(string $startUrl): string;
-
-    public function getNumberOfPages(Document $document): int;
-
-    public function getVacancies(Document $document): void;
-
-    public function getVacancyData($jobLink): array;
-
-    public function saveVacancyToDatabase($data): void;
 
     public function parse(): void;
 
